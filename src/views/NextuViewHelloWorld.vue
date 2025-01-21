306861d487899270ddb8c48b6ff511e5dd9bc9da
Nolan910 <115090897+Nolan910@users.noreply.github.com>
2025-01-21 15:34:09 +0100

    Ex 1

---
+++ src/views/NextuViewHelloWorld.vue
@@ -0,0 +1,26 @@
+<script lang="ts">
+import { ref } from 'vue'
+
+export default {
+  name: 'NextuViewHelloWorld',
+  props:{
+    count: {
+        type: number,
+        default: 2,
+    },
+  },
+  setup(props) {
+    const message = ref('Hi queen')
+    const countData = props.count
+    return {
+      message,
+      countData,
+    }
+  },
+}
+
+</script>
+<template>
+  <div>{{ message }} {{ countData }}</div>
+</template>
+<style></style>
---
+++ src/views/ProductList.vue
@@ -0,0 +1,44 @@
+<script lang="ts">
+//import { ref } from 'vue'
+
+export default {
+  name: 'ProductList',
+  props:{
+    products: {
+        type: Array as () => { id: number; label: string; description: string; status: string}[],
+    },
+  },
+  setup(props) {
+    const productsData = props.products
+    console.log(productsData)
+    return {
+        productsData
+    };
+  }
+}
+
+</script>
+<template>
+  <div>
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Label</th>
+                <th>description</th>
+                <th>Statut</th>
+            </tr>
+        </thead>
+        <body>
+            <tr v-for="productsData in productsData" :key="productsData.id">
+                <td>{{ productsData.id }}</td>
+                <td>{{ productsData.label }}</td>
+                <td>{{ productsData.description }}</td>
+                <td v-if="productsData.quantityData > 0">Disponible</td>
+                <td v-if="productsData.quantityData < 1">Indisponible</td>
+            </tr>
+        </body>
+    </table>
+    </div>
+</template>
+<style></style>
